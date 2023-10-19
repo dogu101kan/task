c6d95e93b4a7718128bf3240e128ec872f9c8892
Dogukan Gazel <dogukan_gazel@windowslive.com>
2023-10-19 23:05:13 +0300

    a

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,35 +1,22 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "../pages/home";
 import Login from "../pages/login";
 import PrivateRoute from "../components/private-route";
 import App from "../App";
-import Notfound from "../pages/notfound"
+import Notfound from "../pages/notfound";
 
-const routes = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        index:true,
-        element: <Login />,
-      },
-      {
-        path: "table",
-        element: <PrivateRoute />,
-        children: [
-          {
-            index: true,
-            element: <Home />,
-          }
-        ],
-      },
-      {
-        path: "*",
-        element: <Notfound />,
-      },
-    ],
-  },
-]);
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Login />} />
+        <Route path="table" element={<PrivateRoute />}>
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="*" element={<Notfound />} />
+      </Route>
+    </Routes>
+  );
+};
 
-export default routes;
+export default AppRoutes;
