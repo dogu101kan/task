b4ef115c17bddfc3a48e8caae25a970aa5bfb282
Dogukan Gazel <dogukan_gazel@windowslive.com>
2023-10-19 23:10:26 +0300

    a

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,24 +5,12 @@ import routes from "./routes";
 import { RouterProvider } from "react-router-dom";
 import { Provider } from 'react-redux'
 import store from './store'
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
-import Login from "./pages/login";
-import PrivateRoute from "./components/private-route";
-import App from "./App";
-import Notfound from "./pages/notfound";
-
+import AppRoutes from './routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route index element={<Login />} />
-        <Route path="table" element={<PrivateRoute />}>
-          <Route index element={<Home />} />
-        </Route>
-        <Route path="*" element={<Notfound />} />
-      </Route>
-    </Routes>
+    <RouterProvider>
+      <AppRoutes />
+    </RouterProvider>
   </Provider>,
 )
