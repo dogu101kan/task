5255eac48541cb0c5abc7c8750a52e3fae87c0c1
Dogukan Gazel <dogukan_gazel@windowslive.com>
2023-10-19 23:14:38 +0300

    aa

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,18 +1,23 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/home";
 import Login from "../pages/login";
 import PrivateRoute from "../components/private-route";
-import App from "../App";
 import Notfound from "../pages/notfound";
+import App from "../App"
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<App />}>
         <Route index element={<Login />} />
-        <Route path="table" element={<PrivateRoute />}>
-          <Route index element={<Home />} />
-        </Route>
+        <Route
+          path="table"
+          element={
+            <PrivateRoute>
+              <Route index element={<Home />} />
+            </PrivateRoute>
+          }
+        />
         <Route path="*" element={<Notfound />} />
       </Route>
     </Routes>
